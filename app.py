7dc0a7f3ed5eb095dfe6a1a7919f629c45a2a95a
Scott Adams <scottadamsx@gmail.com>
2026-02-03 13:45:19 -0330

    Added in a journal page
    there is now a journal page, which does nothing, but allows you to flip between pages. I ran into a bug where my reminders page is not working as expected, trying to figure that out now

--- app.py
+++ app.py
@@ -2,10 +2,20 @@ from flask import Flask, render_template, request, jsonify
 from file import saveReminder, readReminders, saveToJSON, loadFromJSON
 app = Flask(__name__)
 
+# pages 
 @app.route("/")
 def home():
     reminders = loadFromJSON("reminders.json")
     return render_template("index.html",reminders=reminders)
+
+@app.route("/journal")
+def journal():
+    return render_template("journal.html")
+
+@app.route("/reminders")
+def reminder():
+    return render_template("reminders.html")
+
 
 @app.route("/newReminder", methods=["POST"])
 def newReminder():
